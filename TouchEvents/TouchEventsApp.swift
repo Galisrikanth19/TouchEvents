430fddd8c9b29ebbe51ae27f37995aa80edf5b0f
GaliSrikanth <GaliSrikanth19@gmail.com>
2025-04-23 15:41:13 +0530

    Initial Commit

---
+++ TouchEvents/TouchEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchEventsApp.swift
+//  TouchEvents
+//
+//  Created by GaliSrikanth on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
